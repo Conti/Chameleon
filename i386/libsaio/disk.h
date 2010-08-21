e4fb00cb6337fb0f4568dfd245ee97c4b146e52e
zef <zef@b5af796a-00a8-11df-919b-ffff7a100b7d>
2010-08-21 17:01:21 +0000

    Added missing disk.h from previous commit.
    git-svn-id: http://forge.voodooprojects.org/svn/chameleon/trunk@414 b5af796a-00a8-11df-919b-ffff7a100b7d

---
+++ i386/libsaio/disk.h
@@ -0,0 +1,14 @@
+/*
+ *  disk.h
+ *  Chameleon
+ *
+ *  Created by Daniel Miranda on 27/07/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+#ifndef __LIBSAIO_DISK_H
+#define __LIBSAIO_DISK_H
+
+char* matchVolumeToString( BVRef bvr, const char* match, bool matchPartial);
+
+#endif /* __LIBSAIO_DISK_H */
