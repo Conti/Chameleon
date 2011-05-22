c577a325c104ceef3c77c1dc47f2659254ca10c7
meklort <meklort@b5af796a-00a8-11df-919b-ffff7a100b7d>
2011-05-22 08:50:49 +0000

    Set default resolution read from edid
    git-svn-id: http://forge.voodooprojects.org/svn/chameleon/trunk@866 b5af796a-00a8-11df-919b-ffff7a100b7d

--- i386/modules/Resolution/Resolution.c
+++ i386/modules/Resolution/Resolution.c
@@ -4,10 +4,15 @@
  */
 
 #include "915resolution.h"
-
+#include "gui.h"
 
 void Resolution_start()
 {
+    UInt32 bp = 0;
+    UInt32 x, y;
 	patchVideoBios();
+    getResolution(&x, &y, &bp);
+    gui.screen.width = x;
+    gui.screen.height = y;
 }
 
