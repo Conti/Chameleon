ccf2b98b3a5f4c7c3520a3149c9c163016395027
azimutz <azimutz@b5af796a-00a8-11df-919b-ffff7a100b7d>
2011-07-12 19:27:51 +0000

    Style gma.c/h (matches the ones on my branch).
    git-svn-id: http://forge.voodooprojects.org/svn/chameleon/trunk@1150 b5af796a-00a8-11df-919b-ffff7a100b7d

--- i386/libsaio/gma.h
+++ i386/libsaio/gma.h
@@ -8,9 +8,9 @@ struct gma_gpu_t {
 	char *name;
 };
 
-#define REG8(reg)  ((volatile uint8_t *)regs)[(reg)]
-#define REG16(reg)  ((volatile uint16_t *)regs)[(reg) >> 1]
-#define REG32(reg)  ((volatile uint32_t *)regs)[(reg) >> 2]
+#define REG8(reg)	((volatile uint8_t *)regs)[(reg)]
+#define REG16(reg)	((volatile uint16_t *)regs)[(reg) >> 1]
+#define REG32(reg)	((volatile uint32_t *)regs)[(reg) >> 2]
 
 
 #endif /* !__LIBSAIO_GMA_H */
